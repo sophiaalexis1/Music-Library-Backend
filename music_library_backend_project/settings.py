f4d159c36cf29859470704992429f316ffd5ac52
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-15 20:54:57 -0400

    Created music_library Model for music_library App

--- music_library_backend_project/settings.py
+++ music_library_backend_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5(vv!yf0e5d_k2^tml9lz6dr_n^gn5l)m3b=fhmp*t*r_wcdy)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',  
+    'corsheaders',
+    'music_library',  
 ]
 
 MIDDLEWARE = [
@@ -73,11 +73,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'music_library_backend_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 
 
 
