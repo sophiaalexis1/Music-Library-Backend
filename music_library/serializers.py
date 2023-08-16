c6ae27fca7cb73d13f57a14562a17fb7f0960893
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-15 23:07:09 -0400

    created GET and POST commands in Postman

--- music_library/serializers.py
+++ music_library/serializers.py
@@ -4,4 +4,4 @@ from .models import Songs;
 class MusicLibrarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Songs
-        fields = ['title', 'artist', 'album', 'release_date', 'genre']
+        fields = ['id','title', 'artist', 'album', 'release_date', 'genre']
--- music_library/views.py
+++ music_library/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Songs
+from .serializers import MusicLibrarySerializer
 
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def song_list(request):
-    
-    
-    return Response('ok')
+    if request.method == 'GET':
+        songs = Songs.objects.all()
+        serializer = MusicLibrarySerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST': 
+        serializer = MusicLibrarySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
