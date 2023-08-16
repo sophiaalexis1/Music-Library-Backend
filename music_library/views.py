60991424509f2001ca78942bc6e3b6082450cde5
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-15 21:20:54 -0400

    Updated project urls + created music_library urls

--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def song_list(request):
+    
+    
+    return Response('ok')
